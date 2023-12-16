8cf29176780b0d1b029aa4dec8de4eb88d1e50eb
NkamoN <r.nkamo@gmail.com>
2023-12-16 21:08:30 +0200

    5. nop

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+  *f_nop- Does nothing
+  *@head: stack head
+  *@counter: line number
+  *Return: no return
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
